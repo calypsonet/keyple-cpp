c5c93087d42fdcf2f754438e36534e3c7d9fff39
P-ILLINGER <philippe.illinger@wanadoo.fr>
2019-11-25 21:40:04 +0100

    TU calypso suite

--- component/keyple-calypso/src/test/command/po/builder/ReadRecordsCmdBuildTest.cpp
+++ component/keyple-calypso/src/test/command/po/builder/ReadRecordsCmdBuildTest.cpp
@@ -1,27 +1,18 @@
 #include "ReadRecordsCmdBuildTest.h"
-#include "../../../../../../../../../../../keyple-core/src/main/java/org/eclipse/keyple/command/AbstractApduCommandBuilder.h"
-#include "../../../../../../../../../../../keyple-core/src/main/java/org/eclipse/keyple/seproxy/message/ApduRequest.h"
-#include "../../../../../../../../../main/java/org/eclipse/keyple/calypso/command/PoClass.h"
-#include "../../../../../../../../../main/java/org/eclipse/keyple/calypso/command/po/builder/ReadRecordsCmdBuild.h"
 
-namespace org {
-    namespace eclipse {
+using namespace keyple::calypso::command::po::builder;
+
         namespace keyple {
             namespace calypso {
                 namespace command {
                     namespace po {
                         namespace builder {
-                            using PoClass = org::eclipse::keyple::calypso::command::PoClass;
-                            using AbstractApduCommandBuilder = org::eclipse::keyple::command::AbstractApduCommandBuilder;
-                            using ApduRequest = org::eclipse::keyple::seproxy::message::ApduRequest;
-                            using org::junit::Assert;
-                            using org::junit::Test;
-                            using org::junit::runner::RunWith;
-                            using org::mockito::junit::MockitoJUnitRunner;
 
-//JAVA TO C++ CONVERTER TODO TASK: Most Java annotations will not have direct C++ equivalents:
-//ORIGINAL LINE: @Test public void readRecords_rev2_4() throws IllegalArgumentException
-                            void ReadRecordsCmdBuildTest::readRecords_rev2_4() throw(std::invalid_argument) {
+                            using PoClass = keyple::calypso::command::PoClass;
+                            using AbstractApduCommandBuilder = keyple::core::command::AbstractApduCommandBuilder;
+                            using ApduRequest = keyple::core::seproxy::message::ApduRequest;
+
+                            void ReadRecordsCmdBuildTest::readRecords_rev2_4() {
 
                                 char cla = static_cast<char>(0x94);
                                 char cmd = static_cast<char>(0xB2);
@@ -33,12 +24,10 @@ namespace org {
                                 std::vector<char> request2_4 = {cla, cmd, record_number, p2, 0x00};
                                 apduCommandBuilder = std::make_shared<ReadRecordsCmdBuild>(PoClass::LEGACY, sfi, record_number, readJustOneRecord, expectedLength, "TestRev2_4");
                                 apduRequest = apduCommandBuilder->getApduRequest();
-                                Assert::assertArrayEquals(request2_4, apduRequest->getBytes());
+                                ASSERT_EQ(request2_4, apduRequest->getBytes());
                             }
 
-//JAVA TO C++ CONVERTER TODO TASK: Most Java annotations will not have direct C++ equivalents:
-//ORIGINAL LINE: @Test public void readRecords_rev3_1() throws IllegalArgumentException
-                            void ReadRecordsCmdBuildTest::readRecords_rev3_1() throw(std::invalid_argument) {
+                            void ReadRecordsCmdBuildTest::readRecords_rev3_1() {
 
                                 char cla = static_cast<char>(0x00);
                                 char cmd = static_cast<char>(0xB2);
@@ -51,12 +40,10 @@ namespace org {
                                 std::vector<char> request3_1 = {cla, cmd, record_number, p2, 0x00};
                                 apduCommandBuilder = std::make_shared<ReadRecordsCmdBuild>(PoClass::ISO, sfi, record_number, readJustOneRecord, expectedLength, "TestRev3_1");
                                 apduRequest = apduCommandBuilder->getApduRequest();
-                                Assert::assertArrayEquals(request3_1, apduRequest->getBytes());
+                                ASSERT_EQ(request3_1, apduRequest->getBytes());
                             }
 
-//JAVA TO C++ CONVERTER TODO TASK: Most Java annotations will not have direct C++ equivalents:
-//ORIGINAL LINE: @Test public void readRecords_rev3_2() throws IllegalArgumentException
-                            void ReadRecordsCmdBuildTest::readRecords_rev3_2() throw(std::invalid_argument) {
+                            void ReadRecordsCmdBuildTest::readRecords_rev3_2() {
                                 char cla = static_cast<char>(0x00);
                                 char cmd = static_cast<char>(0xB2);
                                 bool readJustOneRecord = false;
@@ -67,12 +54,28 @@ namespace org {
                                 std::vector<char> request3_2 = {cla, cmd, record_number, p2, 0x00};
                                 apduCommandBuilder = std::make_shared<ReadRecordsCmdBuild>(PoClass::ISO, sfi, record_number, readJustOneRecord, expectedLength, "TestRev3_2");
                                 apduRequest = apduCommandBuilder->getApduRequest();
-                                Assert::assertArrayEquals(request3_2, apduRequest->getBytes());
+                                ASSERT_EQ(request3_2, apduRequest->getBytes());
                             }
                         }
                     }
                 }
             }
         }
-    }
+
+TEST(ReadRecordsCmdBuildTest, testA) 
+{
+    std::shared_ptr<ReadRecordsCmdBuildTest> LocalTest = std::make_shared<ReadRecordsCmdBuildTest>();
+    LocalTest->readRecords_rev2_4();
+}
+
+TEST(ReadRecordsCmdBuildTest, testB) 
+{
+    std::shared_ptr<ReadRecordsCmdBuildTest> LocalTest = std::make_shared<ReadRecordsCmdBuildTest>();
+    LocalTest->readRecords_rev3_1();
+}
+
+TEST(ReadRecordsCmdBuildTest, testC) 
+{
+    std::shared_ptr<ReadRecordsCmdBuildTest> LocalTest = std::make_shared<ReadRecordsCmdBuildTest>();
+    LocalTest->readRecords_rev3_2();
 }
--- component/keyple-calypso/src/test/command/po/builder/UpdateRecordCmdBuildTest.h
+++ component/keyple-calypso/src/test/command/po/builder/UpdateRecordCmdBuildTest.h
@@ -4,9 +4,16 @@
 #include <stdexcept>
 #include <memory>
 
-//JAVA TO C++ CONVERTER NOTE: Forward class declarations:
-namespace org { namespace eclipse { namespace keyple { namespace command { class AbstractApduCommandBuilder; } } } }
-namespace org { namespace eclipse { namespace keyple { namespace seproxy { namespace message { class ApduRequest; } } } } }
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+#include "AbstractApduCommandBuilder.h"
+#include "ApduRequest.h"
+#include "PoClass.h"
+#include "UpdateRecordCmdBuild.h"
+
+namespace keyple { namespace core { namespace command { class AbstractApduCommandBuilder; } } }
+namespace keyple { namespace core { namespace seproxy { namespace message { class ApduRequest; } } } }
 
 /********************************************************************************
  * Copyright (c) 2018 Calypso Networks Association https://www.calypsonet-asso.org/
@@ -19,20 +26,16 @@ namespace org { namespace eclipse { namespace keyple { namespace seproxy { names
  *
  * SPDX-License-Identifier: EPL-2.0
  ********************************************************************************/
-namespace org {
-    namespace eclipse {
         namespace keyple {
             namespace calypso {
                 namespace command {
                     namespace po {
                         namespace builder {
 
+                            using AbstractApduCommandBuilder = keyple::core::command::AbstractApduCommandBuilder;
+                            using ApduRequest = keyple::core::seproxy::message::ApduRequest;
 
-                            using AbstractApduCommandBuilder = org::eclipse::keyple::command::AbstractApduCommandBuilder;
-                            using ApduRequest = org::eclipse::keyple::seproxy::message::ApduRequest;
 
-//JAVA TO C++ CONVERTER TODO TASK: Most Java annotations will not have direct C++ equivalents:
-//ORIGINAL LINE: @RunWith(MockitoJUnitRunner.class) public class UpdateRecordCmdBuildTest
                             class UpdateRecordCmdBuildTest : public std::enable_shared_from_this<UpdateRecordCmdBuildTest> {
 
                             private:
@@ -42,27 +45,21 @@ namespace org {
 
                                 std::shared_ptr<AbstractApduCommandBuilder> apduCommandBuilder;
 
-                                std::shared_ptr<ApduRequest> org;
+                                std::shared_ptr<ApduRequest> apduRequest;
 
                             public:
-//JAVA TO C++ CONVERTER TODO TASK: Most Java annotations will not have direct C++ equivalents:
-//ORIGINAL LINE: @Test public void updateRecordCmdBuild_rev2_4() throws IllegalArgumentException
-                                virtual void updateRecordCmdBuild_rev2_4() throw(std::invalid_argument);
 
-//JAVA TO C++ CONVERTER TODO TASK: Most Java annotations will not have direct C++ equivalents:
-//ORIGINAL LINE: @Test public void updateRecordCmdBuild_rev3_1() throws IllegalArgumentException
-                                virtual void updateRecordCmdBuild_rev3_1() throw(std::invalid_argument);
+                                virtual void updateRecordCmdBuild_rev2_4();
 
-//JAVA TO C++ CONVERTER TODO TASK: Most Java annotations will not have direct C++ equivalents:
-//ORIGINAL LINE: @Test public void updateRecordCmdBuild_rev3_2() throws IllegalArgumentException
-                                virtual void updateRecordCmdBuild_rev3_2() throw(std::invalid_argument);
+
+                                virtual void updateRecordCmdBuild_rev3_1();
+
+
+                                virtual void updateRecordCmdBuild_rev3_2();
 
                             };
-
                         }
                     }
                 }
             }
         }
-    }
-}
